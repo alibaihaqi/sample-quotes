9fc3614c94785b27e60b5415807b721e5a18e331
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2022-02-26 20:08:24 +0700

    add sample package

---
+++ favs.go
@@ -0,0 +1,8 @@
+package sample_quotes
+
+func Favs() []string {
+	return []string{
+		"hands-on golang level 1",
+		"learn go module",
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alibaihaqi/sample-quotes
+
+go 1.16
